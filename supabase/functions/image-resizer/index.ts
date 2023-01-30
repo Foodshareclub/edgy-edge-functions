df1c0a1efe86b1632b1dc065abff89b8f2b4bbdd
Tarlan Isaev <tamerlanium@gmail.com>
2023-01-30 13:00:54 +0300

    Added changes 

--- supabase/functions/image-resizer/index.ts
+++ supabase/functions/image-resizer/index.ts
@@ -1,13 +1,15 @@
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { BufReader } from "https://deno.land/std@0.131.0/io/bufio.ts";
 import { readLine } from "https://deno.land/std@0.131.0/io/util.ts";
-
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
+
 const bucketName = 'posts';
+const imageId = '1644347323145739.jpg';
+const imageBuffer = await Deno.readFile("https://iazmjdjwnkilycbjwpzp.supabase.co/storage/v1/object/public/posts/10/1644347323145739.jpg");
 
 const updateImage = async (imageId: string, imageBuffer: Uint8Array) => {
-    const apiKey = config().SUPABASE_API_KEY;
+    const apiKey = config().SUPABASE_ACCESS_TOKEN;
     const response = await fetch(`https://api.supabase.co/bucket/${bucketName}/${imageId}`, {
         method: 'PUT',
         body: imageBuffer,
@@ -21,9 +23,6 @@ const updateImage = async (imageId: string, imageBuffer: Uint8Array) => {
     }
     console.log("Image updated successfully!");
 };
-
-const imageId = '1644347323145739.jpg';
-const imageBuffer = await Deno.readFile("https://iazmjdjwnkilycbjwpzp.supabase.co/storage/v1/object/public/posts/10/1644347323145739.jpg");
 
 try {
     await updateImage(imageId, imageBuffer);
