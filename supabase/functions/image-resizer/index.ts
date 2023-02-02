462009020adc631d0658582597c4d950501ac2a3
Tarlan Isaev <tamerlanium@gmail.com>
2023-02-02 13:06:47 +0300

    Added changes 

--- supabase/functions/image-resizer/index.ts
+++ supabase/functions/image-resizer/index.ts
@@ -1,29 +1,20 @@
-import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
-import { readFileSync } from "https://deno.land/std@0.57.0/fs/read_file.ts";
-import { Sharp } from "https://deno.land/x/sharp/mod.ts";
-
-interface Event {
-  data: any;
-}
-
-export default async ({ event }: { event: Event }) => {
-  // Get the image file from the event object
-  const image = event.data;
+import {
+    ImageMagick,
+    IMagickImage,
+    initializeImageMagick,
+    MagickFormat,
+  } from "https://deno.land/x/imagemagick_deno/mod.ts";
   
-  // Read the image into memory
-  const imageData = readFileSync(image);
-
-  // Create a Sharp instance and resize the image
-  const sharp = new Sharp(imageData);
-  const resizedImage = await sharp.resize({
-    width: 100,
-    height: 100
-  }).toBuffer();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      resizedImage: resizedImage
-    })
-  };
-};
+  await initializeImageMagick(); // make sure to initialize first!
+  
+  const data: Uint8Array = await Deno.readFile("image.jpg");
+  
+  ImageMagick.read(data, (img: IMagickImage) => {
+    img.resize(200, 100);
+    img.blur(20, 6);
+  
+    img.write(
+      (data: Uint8Array) => Deno.writeFile("image-blur.jpg", data),
+      MagickFormat.Jpeg,
+    );
+  });
