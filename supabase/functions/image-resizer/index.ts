a1212e2f107fb91f1c4f48c0970a0ba432219e02
Tarlan Isaev <tamerlanium@gmail.com>
2023-02-02 11:59:05 +0300

    Added changes 

--- supabase/functions/image-resizer/index.ts
+++ supabase/functions/image-resizer/index.ts
@@ -1,4 +1,4 @@
-import { compress } from "https://deno.land/x/compress/mod.ts";
+import { imgproxy } from "https://deno.land/x/imgproxy/mod.ts";
 
 interface Event {
   data: any;
@@ -8,13 +8,15 @@ export default async ({ event }: { event: Event }) => {
   // Get the image file from the event object
   const image = event.data;
   
-  // Compress the image using the compress library
-  const compressedImage = await compress(image);
+  // Generate a URL for the resized image using imgproxy
+  const resizedImageUrl = imgproxy.url(image, {
+    resize: "800x600"
+  });
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      compressedImage: compressedImage
+      resizedImageUrl: resizedImageUrl
     })
   };
 };
